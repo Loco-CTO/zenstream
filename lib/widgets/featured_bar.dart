6d10fdee1ece497cbe40bc262a6af09e22148706
Poco <61045140+Loco-CTO@users.noreply.github.com>
2024-12-14 23:12:20 +0000

    refactor: improve page navigation logic in FeaturedBar widget

--- lib/widgets/featured_bar.dart
+++ lib/widgets/featured_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'dart:async';
-import 'package:zenstream/utils/theme_style.dart';
 
 class FeaturedBar extends StatefulWidget {
   const FeaturedBar({super.key});
@@ -18,18 +17,15 @@ class FeaturedBarState extends State<FeaturedBar> {
   void initState() {
     super.initState();
     _timer = Timer.periodic(const Duration(seconds: 5), (Timer timer) {
-      if (_pageController.page == 3) {
-        _pageController.animateToPage(
-          0,
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeIn,
-        );
-      } else {
-        _pageController.nextPage(
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeIn,
-        );
+      int nextPage = (_pageController.page?.toInt() ?? 0) + 1;
+      if (nextPage >= 4) {
+        nextPage = 0;
       }
+      _pageController.animateToPage(
+        nextPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
     });
   }
 
@@ -72,7 +68,7 @@ class FeaturedBarState extends State<FeaturedBar> {
               controller: _pageController,
               count: 4,
               effect: SlideEffect(
-                activeDotColor: ThemeDataStyle.dark.primaryColor,
+                activeDotColor: Colors.blue,
                 dotColor: Colors.grey,
                 dotHeight: 10,
                 dotWidth: 10,

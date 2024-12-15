cb58945ebb378944feaaa63f999c250e1e318dff
Poco <61045140+Loco-CTO@users.noreply.github.com>
2024-12-15 13:02:24 +0000

    feat: implement theme persistence using shared_preferences

--- lib/utils/theme_notifier.dart
+++ lib/utils/theme_notifier.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.light;
 
+  ThemeNotifier() {
+    _loadTheme();
+  }
+
   ThemeMode get themeMode => _themeMode;
 
-  void toggleTheme() {
+  void toggleTheme() async {
     if (_themeMode == ThemeMode.light) {
       _themeMode = ThemeMode.dark;
     } else {
       _themeMode = ThemeMode.light;
     }
     notifyListeners();
+    _saveTheme();
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt('themeMode') ?? 0;
+    _themeMode = ThemeMode.values[themeIndex];
+    notifyListeners();
+  }
+
+  void _saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('themeMode', _themeMode.index);
   }
 }

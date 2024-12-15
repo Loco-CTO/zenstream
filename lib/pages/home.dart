5be8f4a9bb2f322d19d94a98a29cd09a9805d971
Poco <61045140+Loco-CTO@users.noreply.github.com>
2024-12-15 12:50:31 +0000

    refactor: rename _HomePageState to HomePageState and remove unused ListPage navigation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:zenstream/widgets/layout.dart';
-import 'package:zenstream/pages/list.dart';
 import 'package:zenstream/widgets/featured_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return LayoutScaffold(
@@ -21,15 +20,6 @@ class _HomePageState extends State<HomePage> {
             const FeaturedBar(),
             const SizedBox(height: 20),
             const Text('ようだい！'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ListPage()),
-                );
-              },
-              child: const Text('Go to List Page'),
-            ),
           ],
         ),
       ),

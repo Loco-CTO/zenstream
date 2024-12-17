691bbf89053a90b63d1dce518ab626f2ca9bc613
Poco <61045140+Loco-CTO@users.noreply.github.com>
2024-12-17 17:04:04 +0000

    feat: add LoginScreen with authentication functionality and UI layout

---
+++ lib/pages/login.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zenstream/widgets/base_layout.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String? _errorMessage;
+
+  Future<void> _login() async {
+    final url = '${dotenv.env['WEB_URL']}/Users/AuthenticateByName';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: jsonEncode({
+        'Username': _usernameController.text,
+        'Pw': _passwordController.text,
+      }),
+    );
+
+    print(url);
+    print(_usernameController.text);
+    print(_passwordController.text);
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final token = jsonDecode(response.body)['AccessToken'];
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token);
+      Navigator.of(context).pushReplacementNamed('/home');
+    } else {
+      setState(() {
+        _errorMessage = 'Login failed. Please check your credentials.';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return BaseLayout(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(50.0),
+            decoration: BoxDecoration(
+              color: theme.colorScheme.surfaceDim,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.0,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icons/icon.png',
+                  width: 100,
+                  height: 100,
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: SizedBox(
+                    width: 350,
+                    child: TextField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(labelText: 'Username'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 350,
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _login,
+                  child: const Text('Login'),
+                ),
+                if (_errorMessage != null) ...[
+                  const SizedBox(height: 20),
+                  Text(
+                    _errorMessage!,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ],
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

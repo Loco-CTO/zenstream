0000cdccb1a2477328c816e5ac5bbfd24aeb60cc
Poco <61045140+Loco-CTO@users.noreply.github.com>
2024-12-17 16:24:42 +0000

    feat: convert SplashScreen to StatefulWidget and implement navigation to HomePage after delay

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:zenstream/pages/home.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

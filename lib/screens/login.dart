907a335338f19b5aad2cd9b7803f6a251f864cca
Poco <61045140+Loco-CTO@users.noreply.github.com>
2024-12-18 19:34:23 +0000

    hore: backport login.dart to commit f48a6ce

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zenstream/widgets/base_layout.dart';
-import 'package:zenstream/jellyfin/api_services.swagger.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,25 +15,36 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final FocusNode _usernameFocusNode = FocusNode();
-  final FocusNode _passwordFocusNode = FocusNode();
   String? _errorMessage;
 
   Future<void> _login() async {
-    final apiService = JellyfinApiService();
-    try {
-      final response = await apiService.authenticateByName(
-        _usernameController.text,
-        _passwordController.text,
-      );
+    final url = '${dotenv.env['WEB_URL']}/Users/AuthenticateByName';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: jsonEncode({
+        'Username': _usernameController.text,
+        'Pw': _passwordController.text,
+      }),
+    );
 
-      final token = response['AccessToken'];
+    print(url);
+    print(_usernameController.text);
+    print(_passwordController.text);
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final token = jsonDecode(response.body)['AccessToken'];
       final prefs = await SharedPreferences.getInstance();
       await prefs.setString('token', token);
       if (mounted) {
         Navigator.of(context).pushReplacementNamed('/home');
       }
-    } catch (e) {
+    } else {
       setState(() {
         _errorMessage = 'Login failed. Please check your credentials.';
       });
@@ -43,73 +56,67 @@ class LoginScreenState extends State<LoginScreen> {
     final theme = Theme.of(context);
 
     return BaseLayout(
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: theme.colorScheme.surface,
-            borderRadius: BorderRadius.circular(16.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: 10.0,
-                offset: Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Login',
-                style: theme.textTheme.headlineMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(50.0),
+            decoration: BoxDecoration(
+              color: theme.colorScheme.surfaceDim,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.0,
+                  offset: Offset(0, 5),
                 ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _usernameController,
-                focusNode: _usernameFocusNode,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  errorText: _errorMessage,
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icons/icon.png',
+                  width: 100,
+                  height: 100,
                 ),
-                onSubmitted: (_) {
-                  _passwordFocusNode.requestFocus();
-                },
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _passwordController,
-                focusNode: _passwordFocusNode,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  errorText: _errorMessage,
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: SizedBox(
+                    width: 350,
+                    child: TextField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(labelText: 'Username'),
+                    ),
+                  ),
                 ),
-                obscureText: true,
-                onSubmitted: (_) {
-                  _login();
-                },
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _login,
-                child: Text('Login'),
-              ),
-            ],
+                SizedBox(
+                  width: 350,
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _login,
+                  child: const Text('Login'),
+                ),
+                if (_errorMessage != null) ...[
+                  const SizedBox(height: 20),
+                  Text(
+                    _errorMessage!,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ],
+              ],
+            ),
           ),
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    _usernameFocusNode.dispose();
-    _passwordFocusNode.dispose();
-    super.dispose();
   }
 }

83c197d94e1bc25ef13b4b4fed5900a2ed49f91d
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-13 23:52:51 +0000

    refactor: improve PreCheckState navigation and update layout to use LayoutScaffold

--- lib/utils/precheck.dart
+++ lib/utils/precheck.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:shared_preferences/shared_preferences.dart";
 import "package:zenstream/jellyfin/api_services.swagger.dart";
+import "package:zenstream/widgets/base_layout.dart";
+import "package:zenstream/widgets/layout.dart";
 
 class PreCheck extends StatefulWidget {
   final Widget nextPage;
@@ -29,11 +31,11 @@ class PreCheckState extends State<PreCheck> {
       setState(() {
         _errorMessage = "You have been logged out";
       });
-      WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
         Navigator.pushNamed(context, '/login');
-      });
+      }
     } else {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
         Navigator.of(context).pushReplacement(
           PageRouteBuilder(
             pageBuilder: (context, animation, secondaryAnimation) =>
@@ -41,21 +43,20 @@ class PreCheckState extends State<PreCheck> {
             transitionDuration: Duration.zero,
           ),
         );
-      });
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _errorMessage != null
-            ? Text(
-                _errorMessage!,
-                style: const TextStyle(color: Colors.red),
-              )
-            : const CircularProgressIndicator(),
-      ),
-    );
+    return LayoutScaffold(
+        body: Center(
+      child: _errorMessage != null
+          ? Text(
+              _errorMessage!,
+              style: const TextStyle(color: Colors.red),
+            )
+          : const CircularProgressIndicator(),
+    ));
   }
 }

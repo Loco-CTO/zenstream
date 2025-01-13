571739ed56a0a3a066f3200ad8037b110bcad4b1
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-13 19:13:44 +0000

    fix: update image URLs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,7 +3,9 @@ import 'package:zenstream/widgets/layout.dart';
 import 'package:zenstream/widgets/featured_bar.dart';
 import 'package:zenstream/widgets/series/item_banner.dart';
 import 'package:zenstream/widgets/series/scroller.dart';
+import 'package:zenstream/jellyfin/api_services.swagger.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +15,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  final JellyfinApiService _apiService = JellyfinApiService();
+  List<dynamic> _libraries = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchLibraries();
+  }
+
+  Future<void> _fetchLibraries() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    if (token != null) {
+      final libraries = await _apiService.getUserLibraries(token);
+      setState(() {
+        _libraries = libraries;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutScaffold(
@@ -45,7 +68,7 @@ class HomeScreenState extends State<HomeScreen> {
             margin: const EdgeInsets.all(8),
             child: ItemBanner(
               imageUrl:
-                  'http://localhost:8096/Items/329e09da86188f42c1f304be2a60946a/Images/Primary?fillHeight=656&fillWidth=446&quality=96&tag=31562f151b0cfa4e0c98801a022928d4',
+                  'http://https://theatre.lococto.me:8096/Items/329e09da86188f42c1f304be2a60946a/Images/Primary?fillHeight=656&fillWidth=446&quality=96&tag=31562f151b0cfa4e0c98801a022928d4',
               title: 'コードギアス 反逆のルルーシュ',
               subtitle: 'シーズン2 第一話：超合集国決議第壱號 ',
             ),

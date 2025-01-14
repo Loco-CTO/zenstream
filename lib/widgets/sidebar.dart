5ec31cb3ef96d3794fe5d8c45870ae6a925853b4
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-14 00:09:50 +0000

    refactor: simplify Sidebar widget by removing unused components and enhancing title bar layout

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,8 +1,5 @@
 import "package:flutter/material.dart";
-import "package:bitsdojo_window/bitsdojo_window.dart";
-import "package:provider/provider.dart";
-import "package:shared_preferences/shared_preferences.dart";
-import "package:zenstream/utils/theme_notifier.dart";
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 class Sidebar extends StatelessWidget {
   const Sidebar({super.key});
@@ -11,71 +8,45 @@ class Sidebar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final sidebarColor = theme.colorScheme.surfaceDim;
-    final iconColor = theme.iconTheme.color ?? Colors.black;
 
     return Container(
       width: 92,
       decoration: BoxDecoration(color: sidebarColor),
       child: Column(
         children: [
-          _buildTitleBar(),
-          const Spacer(),
-          _buildThemeToggleButton(context, iconColor),
-          _buildLogoutButton(context, iconColor),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTitleBar() {
-    return WindowTitleBarBox(
-      child: Row(
-        children: [
-          Expanded(
-            child: MoveWindow(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 8, 0, 0),
-                child: Text(
-                  "ZenStream",
-                  style: TextStyle(
-                    fontSize: 13.0,
-                    fontFamily: "Nunito",
-                    fontWeight: FontWeight.w500,
+          Container(
+            height: 40,
+            child: WindowTitleBarBox(
+              child: MoveWindow(
+                child: Center(
+                  child: Text(
+                    "ZenStream",
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      fontFamily: "Nunito",
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
               ),
             ),
           ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: IconButton(
+              icon: Icon(Icons.widgets_rounded,
+                  color: Theme.of(context)
+                      .colorScheme
+                      .outline
+                      .withAlpha((0.5 * 255).toInt())),
+              iconSize: 40.0,
+              onPressed: () {
+                // TODO: Handle Menu
+              },
+            ),
+          ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildThemeToggleButton(BuildContext context, Color iconColor) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20.0),
-      child: IconButton(
-        icon: Icon(Icons.brightness_6, color: iconColor),
-        iconSize: 25.0,
-        onPressed: () {
-          Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
-        },
-      ),
-    );
-  }
-
-  Widget _buildLogoutButton(BuildContext context, Color iconColor) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20.0),
-      child: IconButton(
-        icon: Icon(Icons.logout, color: iconColor),
-        iconSize: 25.0,
-        onPressed: () async {
-          final prefs = await SharedPreferences.getInstance();
-          await prefs.remove("token");
-          if (!context.mounted) return;
-          Navigator.pushNamed(context, '/login');
-        },
       ),
     );
   }

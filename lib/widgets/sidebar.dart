13e48794dd31a775257bddbb4a7ac7d515f9710f
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-18 15:00:26 +0000

    refactor: convert Sidebar widget to StatefulWidget and add animations for icon button

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,13 +1,44 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 
-class Sidebar extends StatelessWidget {
+class Sidebar extends StatefulWidget {
   const Sidebar({super.key});
+
+  @override
+  SidebarState createState() => SidebarState();
+}
+
+class SidebarState extends State<Sidebar> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 350),
+      vsync: this,
+    );
+    _rotationAnimation = Tween<double>(begin: 0, end: 0.785398).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutQuint),
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutQuint),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final sidebarColor = theme.colorScheme.surfaceDim;
+    final iconColor = theme.colorScheme.primary;
 
     return Container(
       width: 92,
@@ -34,16 +65,34 @@ class Sidebar extends StatelessWidget {
           const Spacer(),
           Padding(
             padding: const EdgeInsets.only(bottom: 20.0),
-            child: IconButton(
-              icon: Icon(Icons.widgets_rounded,
-                  color: Theme.of(context)
-                      .colorScheme
-                      .outline
-                      .withAlpha((0.5 * 255).toInt())),
-              iconSize: 40.0,
-              onPressed: () {
-                // TODO: Handle Menu
-              },
+            child: MouseRegion(
+              child: MouseRegion(
+                onEnter: (_) {
+                  _controller.forward();
+                },
+                onExit: (_) {
+                  _controller.reverse();
+                },
+                child: AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) {
+                    return Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()
+                        ..rotateZ(_rotationAnimation.value)
+                        ..scale(_scaleAnimation.value),
+                      child: IconButton(
+                        hoverColor: Colors.transparent,
+                        icon: Icon(Icons.widgets_rounded, color: iconColor),
+                        iconSize: 35.0,
+                        onPressed: () {
+                          // TODO: Handle Menu
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
           ),
         ],
